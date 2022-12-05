76ec3092bb9e516228071d02e3316861bd4bd8c9
Emilia Hansson <eh222yn@student.lnu.se>
2022-12-05 15:53:44 +0100

    Create basic service and add k8s

---
+++ litter-service/src/routes/router.js
@@ -0,0 +1,11 @@
+import express from 'express'
+// import createError from 'http-errors'
+// import { router as v1Router } from './api/v1/router.js'
+
+export const router = express.Router()
+
+router.get('/', (req, res, next) => res.json({ "msg": "Hello, World!" }))
+
+// router.use('/api/v1', v1Router)
+
+// router.use('*', (req, res, next) => next(createError(404)))
