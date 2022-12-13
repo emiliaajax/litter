c9c1007a58c4e3463e1606ea00fe7979bf7efe10
af222xh <af222xh@student.lnu.se>
2022-12-13 20:15:29 +0100

    Add main router

---
+++ auth-service/src/routes/router.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import createError from 'http-errors'
+import { router as v1Router } from './api/v1/router.js'
+
+export const router = express.Router()
+
+// Default api route.
+router.use('/api', v1Router)
+
+router.use('/api/v1', v1Router)
+
+// 404 routes.
+router.use('*', (req, res, next) => next(createError(404)))
