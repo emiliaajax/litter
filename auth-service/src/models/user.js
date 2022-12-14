479cbc8ada13197c54b3dbf87f5aea2857538062
af222xh <af222xh@student.lnu.se>
2022-12-14 01:59:19 +0100

    Fix lint errors

--- auth-service/src/models/user.js
+++ auth-service/src/models/user.js
@@ -24,10 +24,10 @@ const schema = new mongoose.Schema({
 schema.set('timestamps', true)
 schema.set('toJSON', {
   virtuals: true,
-  transform(doc, ret) {
+  transform (doc, ret) {
     delete ret.__v
     delete ret._id
-  },
+  }
 })
 
 schema.virtual('id').get(function () {
