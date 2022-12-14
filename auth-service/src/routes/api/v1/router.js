34ea68987d2aae4d748f2ab0a05e3409c981ef00
af222xh <af222xh@student.lnu.se>
2022-12-14 03:48:08 +0100

    Add auth route

--- auth-service/src/routes/api/v1/router.js
+++ auth-service/src/routes/api/v1/router.js
@@ -1,5 +1,6 @@
 import express from 'express'
+import { router as accountRouter } from './account-router.js'
 
 export const router = express.Router()
 
-router.get('/', (req, res) => res.json({ message: 'hello world' }))
+router.use('/auth', accountRouter)
