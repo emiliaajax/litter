378ac50bb975599fb7ab86f265732f392ac7e0e0
Emilia Hansson <eh222yn@student.lnu.se>
2022-12-14 15:41:34 +0100

    Add LitForm component

--- litter-client/src/components/Home/Home.js
+++ litter-client/src/components/Home/Home.js
@@ -1,13 +1,17 @@
 import LitterBox from '../LitterBox/LitterBox.js'
 import FollowingFeed from '../FollowingFeed/FollowingFeed.js'
-import { Grid } from '@mui/material'
+import { Grid, Stack } from '@mui/material'
+import LitForm from '../LitForm/LitForm.js'
 
 const Home = () => {
 
   return ( 
     <Grid container>
       <Grid item xs={9} sx={{ borderRight: '1px solid #dcdcdc' }}>
-        <LitterBox />
+        <Stack>
+          <LitForm />
+          <LitterBox />
+        </Stack>
       </Grid>
       <Grid item xs={3}>
         <FollowingFeed />
