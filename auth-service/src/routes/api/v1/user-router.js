c1cd872391db8f096117b17321e5595fd6f80c1b
af222xh <af222xh@student.lnu.se>
2022-12-15 21:20:00 +0100

    Add user router

---
+++ auth-service/src/routes/api/v1/user-router.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
+import { FollowingController } from '../../../controllers/following-controller.js'
+
+export const router = express.Router()
+
+const controller = new FollowingController()
+const publicKey = Buffer.from(process.env.JWT_PUBLIC_KEY || '', 'base64').toString('utf-8')
+
+/**
+ * Verifies the bearer token JWT.
+ *
+ * @param {object} req The request object.
+ * @param {object} res The response object.
+ * @param {Function} next The next middleware.
+ */
+function authenticateJWT (req, res, next) {
+  if (!req.headers.authorization) return next(createError(401))
+  const [ type, token ] = req.headers.authorization?.split(' ')
+  if (!type || type !== 'Bearer') return next(createError(401))
+
+  try {
+    const payload = jwt.verify(token, publicKey, {algorithm: 'RS256'})
+    req.user = { id: payload.id }
+  } catch (err) {
+    console.log(err)
+    return next(createError(403))
+  }
+
+  next()
+}
+
+router.post('/:id/followings',
+  authenticateJWT,
+  (req, res, next) => controller.create(req, res, next)
+  )
+
+router.get('/:id/followings', (req, res, next) => controller.findAll(req, res, next))
