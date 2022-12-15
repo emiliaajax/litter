0806d17a047b7ec8cabb55d4e4f59c4b416002cb
af222xh <af222xh@student.lnu.se>
2022-12-15 19:23:22 +0100

    Fix order of routes

--- auth-service/src/routes/router.js
+++ auth-service/src/routes/router.js
@@ -4,10 +4,10 @@ import { router as v1Router } from './api/v1/router.js'
 
 export const router = express.Router()
 
+router.use('/api/v1', v1Router)
+
 // Default api route.
 router.use('/api', v1Router)
-
-router.use('/api/v1', v1Router)
 
 // 404 routes.
 router.use('*', (req, res, next) => next(createError(404)))
