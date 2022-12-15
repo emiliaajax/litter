3e854a439954a92d62901d51749c21e70c6286df
Oliwer <oe222ez@student.lnu.se>
2022-12-15 18:17:52 +0100

    Add routes for lits-service

--- litter-service/src/controllers/lits-controller.js
+++ litter-service/src/controllers/lits-controller.js
@@ -1,11 +1,30 @@
 import createError from 'http-errors'
 
 export class LitsController {
-  hello (req, res, next) {
+  getLatestLits (req, res, next) {
     try {
-      res.json({ message: 'Hello from LitsController' })
+      res.json({ message: 'latest lits' })
+    } catch (err) {
+      next(createError(500))
+    }
+  }
+
+  getLitById (req, res, next) {
+    try {
+      res.json({ message: 'lit by id' })
+    } catch (err) {
+      next(createError(500))
+    }
+  }
+
+  createLit (req, res, next) {
+    try {
+      res.json({ message: 'Create Lit' })
     } catch (err) {
       next(createError(500))
     }
   }
 }
+
+
+// http://lits-service/lists/api/v1/lits
