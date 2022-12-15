f62c0cb79ddcac26d36ef125d30922499ea33b9e
af222xh <af222xh@student.lnu.se>
2022-12-15 21:19:31 +0100

    Add user route

--- auth-service/src/routes/api/v1/router.js
+++ auth-service/src/routes/api/v1/router.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import { router as accountRouter } from './account-router.js'
+import { router as userRouter } from './user-router.js'
 
 export const router = express.Router()
 
 router.use('/auth', accountRouter)
+
+router.use('/users', userRouter)
