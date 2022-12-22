12ecb34a0690bda5c8bb1bd1c80397139277986e
af222xh <af222xh@student.lnu.se>
2022-12-22 18:30:26 +0100

    Remove following router

--- auth-service/src/routes/api/v1/router.js
+++ auth-service/src/routes/api/v1/router.js
@@ -1,12 +1,9 @@
 import express from 'express'
 import { router as userRouter } from './user-router.js'
 import { router as accountRouter } from './account-router.js'
-import { router as followingRouter } from './following-router.js'
 
 export const router = express.Router()
 
 router.use('/auth', accountRouter)
-
-router.use('/users/:id/followings', followingRouter)
 
 router.use('/users', userRouter)
